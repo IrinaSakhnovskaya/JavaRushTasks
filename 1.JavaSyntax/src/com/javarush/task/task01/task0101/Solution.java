1f9e07a1732dbf709ad9375c9b0930624c9c178b
User <sakhnovskaya@mail.ru>
2024-03-02 19:35:25 +0300

    add solution JavaSyntax task0101

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0101;
+
+/* 
+Я думаю, быть программистом круто
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // выводим фразу "Я думаю, быть программистом круто" на экран,
+        // кавычки выведены не будут
+        System.out.print("Я думаю, быть программистом круто");
+    }
+}
